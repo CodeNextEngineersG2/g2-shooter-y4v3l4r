a7b8034b515ce0d9d8a70c3235c47dfe8e485569
ahob85 <aaron@ahob85.com>
2018-02-20 15:55:55 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+// UI Variables
+var canvas;
+var gameScreen;
+var gameOverScreen;
+var playAgainButton;
+var scoreDisplays;
+
+// Game Variables
+var gameRunning;
+var shipShooting;
+var alienShooting;
+var score;
+
+// Ship Variables
+var shipDiameter;
+var shipX;
+var shipY;
+var shipSpeed;
+var shipColor;
+
+// Bullet Variables
+var bulletDiameter;
+var bulletX;
+var bulletY;
+
+// Alien Variables
+var alienDiameter;
+var alienX;
+var alienY;
+var alienVelocity;
+
+// Alien Bullet Variables
+var alienBulletDiameter;
+var alienBulletX;
+var alienBulletY;
+
+
+/*
+ * setup()
+ * This function is called once. Sets up the canvas, access HTML elements with
+ * select(), and adds event listeners to those elements. Sets initial values of
+ * variables.
+ */
+
+
+/*
+ * gameOver()
+ * This function stops the game from running, hides the game screen, and shows
+ * the game over screen.
+ */
+
+
+/*
+ * resetGame()
+ * This function "resets the game" by initializing ship, alien, and game
+ * variables, hiding gameOverScreen, and showing the gameScreen.
+ */
+
+
+/*
+ * updateScoreDisplays()
+ * This function simply updates all of the HTML elements that display the score.
+ */
+
+
+/*
+ * draw()
+ * This function animates the ship, alien, and both kinds of bullets.
+ */
+
+
+/*
+ * drawShip()
+ * This function draws the player's ship. It also controls the ship's
+ * x value by checking if the player is holding down the left or right keys.
+ */
+
+
+/*
+ * keyPressed()
+ * This function runs automatically when the player presses the spacebar
+ * (keyCode === 32). If they do, and a bullet is not currently being fired
+ * ("shipShooting" variable is false), it positions the bullet relative to the
+ * ship. Then it sets the "shipShooting" variable to "true", indicating a ship
+ * bullet is currently being fired.
+ */
+
+
+/*
+ * drawBullet()
+ * This function draws a bullet. It also checks to see if the bullet has hit
+ * the alien. If it has, the alien is reset to the top-left of the screen
+ * and the player earns a point. The alien aslo becomes faster (i.e., harder
+ * to hit) each time it is hit by a bullet.
+ */
+
+
+/*
+ * drawAlien()
+ * This function draws an alien. It also checks to see if the alien has touched
+ * the player's ship. If it has, the function calls gameOver().
+ */
+
+
+/*
+ * drawAlienBullet()
+ * This function behaves much like drawBullet(), only it fires from the alien
+ * and not the player's ship. If the bullet hits the player, it's game over.
+ */
+
+
+/*
+ * resetAlien()
+ * This function sets the alien to its original position at the top-left of
+ * the screen. It also sets its velocity to its absolute value (so, if the
+ * velocity was negative when it died, it becomes positive upon reset, making
+ * it always start by moving to the right).
+ */
+
+
+/*
+ * checkCollision(aX, aY, aD, bX, bY, bD)
+ * This function first calculates the distance between two circles based on
+ * their X and Y values. Based on the distance value, the function returns
+ * "true" if the circles are touching, and false otherwise.
+ * Circles are considered touching if
+ * (distance <= (circle1Diameter + circle2Diameter) / 2)
+ */
